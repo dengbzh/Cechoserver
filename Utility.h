3274d0556a647b6f843fbd672f5b9b90e9c752d2
deng <dengbzhy@qq.com>
2016-01-12 01:33:03 -0800

    basic files

---
+++ Utility.h
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Utility.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/20/2015 12:06:44 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dengbzh (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef _UTILITY_H
+#define _UTILITY_H
+#include<pthread.h>
+#include<vector>
+#include<semaphore.h>
+#include<assert.h>
+#include<algorithm>
+#include<fcntl.h>
+#include<sys/mman.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include"apue.h"
+using namespace std;
+
+
+#endif
